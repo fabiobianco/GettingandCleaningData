e1c87237cddc9a712ab6c91a1f8f15ad35cde839
fabiobianco <fbianchini@gmail.com>
2015-04-21 00:01:27 +0200

    scripi update

--- CodeBook.md
+++ CodeBook.md
@@ -1,1 +1,3 @@
-This is the CodeBook...
+This is the CodeBook for the HAR_dataset tiny dataset creared with the run_analysis scrip
+
+SUBJECT: identifies the subject who performed the activity. Its rangr from 1 to 30.
--- run_analysis.R
+++ run_analysis.R
@@ -7,22 +7,45 @@
 #
 
 # 1 - Merges the training and the test sets to create one data set.
-
+#
+activity_labels <- read.table("~/GettingandCleaningData/UCI HAR Dataset/activity_labels.txt", quote="\"")
+activity_labels$V2 <- as.character(activity_labels$V2)
+names(activity_labels)[1] <- "CLASS LABEL"
+names(activity_labels)[2] <- "ACTIVITY NAME"
+#
+features <- read.table("~/GettingandCleaningData/UCI HAR Dataset/features.txt", quote="\"")
+features$V2 <- as.character(features$V2)
+#
 # Read the two datasets (training and test one)
 # 
 # Read "train" data
+subject_train <- read.table("~/GettingandCleaningData/UCI HAR Dataset/train/subject_train.txt", quote="\"")
 X_train <- read.table("~/GettingandCleaningData/UCI HAR Dataset/train/X_train.txt", quote="\"")
 y_train <- read.table("~/GettingandCleaningData/UCI HAR Dataset/train/y_train.txt", quote="\"")
+# Assign names to columns
+names(subject_train) <- "SUBJECT"
+names(y_train) <- "CLASS LABEL"
+names(X_train) <- features[,2]
 #
 # Read "test" data
+subject_test <- read.table("~/GettingandCleaningData/UCI HAR Dataset/test/subject_test.txt", quote="\"")
 X_test <- read.table("~/GettingandCleaningData/UCI HAR Dataset/test/X_test.txt", quote="\"")
 y_test <- read.table("~/GettingandCleaningData/UCI HAR Dataset/test/y_test.txt", quote="\"")
+# Assign names to columns
+names(subject_test) <- "SUBJECT"
+names(y_test) <- "CLASS LABEL"
+names(X_test) <- features[,2]
 #
 # Merge the training and test dataset
 #
 test_dataset <- cbind(subject_test, y_test, X_test)
 train_dataset <- cbind(subject_train, y_train, X_train)
 HAR_dataset <- rbind(test_dataset, train_dataset)
-
+#
+HAR_dataset <- merge(activity_labels,HAR_dataset)
 #
 # 2 - 
+# 3 -
+# 4 -
+#
+# 5 - 
