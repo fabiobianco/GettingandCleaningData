f845d2b722e07c4b77376fcb81ba72e678c4918d
fabiobianco <fbianchini@gmail.com>
2015-04-26 11:14:23 +0200

    Final version!

--- run_analysis.R
+++ run_analysis.R
@@ -6,6 +6,10 @@
 # 4 - Appropriately labels the data set with descriptive variable names.                                                                                #
 # 5 - From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.    #
 #########################################################################################################################################################
+# 
+#Charge the library needed for the script
+#
+library(data.table)
 #
 #####################################################################
 # 1 - Merges the training and the test sets to create one data set. #
@@ -13,22 +17,22 @@
 #
 # read the "activity_label file" and rename the columns
 #
-activity_labels <- read.table("~/GettingandCleaningData/UCI HAR Dataset/activity_labels.txt", quote="\"")
+activity_labels <- read.table("~/GettingandCleaningData/activity_labels.txt", quote="\"")
 activity_labels$V2 <- as.character(activity_labels$V2)
 names(activity_labels)[1] <- "classlabel"
 names(activity_labels)[2] <- "activityname"
 #
 #read the "features" file and rename the columns
-features <- read.table("~/GettingandCleaningData/UCI HAR Dataset/features.txt", quote="\"")
+features <- read.table("~/GettingandCleaningData/features.txt", quote="\"")
 features$V2 <- as.character(features$V2)
 names(features)[2] <- "varnum"
 names(features)[2] <- "varname"
 # 
 # Read "train" data and create the first dataset
 #
-subject_train <- read.table("~/GettingandCleaningData/UCI HAR Dataset/train/subject_train.txt", quote="\"")
-X_train <- read.table("~/GettingandCleaningData/UCI HAR Dataset/train/X_train.txt", quote="\"")
-y_train <- read.table("~/GettingandCleaningData/UCI HAR Dataset/train/y_train.txt", quote="\"")
+subject_train <- read.table("~/GettingandCleaningData/subject_train.txt", quote="\"")
+X_train <- read.table("~/GettingandCleaningData/X_train.txt", quote="\"")
+y_train <- read.table("~/GettingandCleaningData/y_train.txt", quote="\"")
 # Assign names to columns
 names(subject_train) <- "subject"
 names(y_train) <- "classlabel"
@@ -39,9 +43,9 @@ names(X_train) <- features[,2]
 train_dataset <- cbind(subject_train, y_train, X_train)
 #
 # Read "test" data
-subject_test <- read.table("~/GettingandCleaningData/UCI HAR Dataset/test/subject_test.txt", quote="\"")
-X_test <- read.table("~/GettingandCleaningData/UCI HAR Dataset/test/X_test.txt", quote="\"")
-y_test <- read.table("~/GettingandCleaningData/UCI HAR Dataset/test/y_test.txt", quote="\"")
+subject_test <- read.table("~/GettingandCleaningData/subject_test.txt", quote="\"")
+X_test <- read.table("~/GettingandCleaningData/X_test.txt", quote="\"")
+y_test <- read.table("~/GettingandCleaningData/y_test.txt", quote="\"")
 # Assign names to columns
 names(subject_test) <- "subject"
 names(y_test) <- "classlabel"
@@ -72,13 +76,14 @@ HAR_mean_sd_dataset <- cbind(HAR_dataset[,1:3],HAR_dataset[mean_sd_var])
 HAR_dataset1 <- merge(activity_labels,HAR_mean_sd_dataset)
 HAR_dataset1 <- HAR_dataset1[with(HAR_dataset1, order(subject, classlabel)), ]
 # 
-HAR_dataset2 <- HAR_dataset1
-#
 # Delete the "classlabel" column because have the same sense of "activityname" column, and rearrange the columns
 #
 HAR_dataset1 <- cbind(HAR_dataset1[,3],HAR_dataset1[,2],HAR_dataset1[,4:90])
 names(HAR_dataset1)[1] <- "subject"
 names(HAR_dataset1)[2] <- "activityname"
+#
+# Copy the dataset so 
+HAR_dataset2 <- HAR_dataset1
 #
 ###########################################################
 # 4 - labels the data set with descriptive variable names #
@@ -90,26 +95,22 @@ names(HAR_dataset1)[2] <- "activityname"
 # 5 - creates a second, independent tidy dataset with the average of each variable for each activity and each subject #
 #######################################################################################################################
 #
-# create a list by splitting the dataset by "subject" and "classlabel"
+# create a list by splitting the dataset by "subject" 
 #
-#HAR_final <- split(HAR_dataset2, list(HAR_dataset2$subject, HAR_dataset2$classlabel))
 HAR_final <- split(HAR_dataset2, HAR_dataset2$subject)
-# aaa <- lapply(HAR_final, function(x) colMeans(x[,mean_sd_var]))
-# 
-# df <- data.frame(matrix(unlist(aaa), nrow=180, byrow=T),stringsAsFactors=FALSE)
 #
-HAR_dataset3 <- c()
+# For every single subject calculate the mean by activity name
 #
+HAR_dataset3 <- data.frame()
 for (i in 1:30){
-        temp <- by(HAR_final[[i]], HAR_final[[i]][,1], function(x) mean(x))
-        #apply(HAR_final[[i]],2,mean)
-        #temp <- lapply(HAR_final[[i]], mean)
-        HAR_dataset3 <- rbind(HAR_dataset3,temp)
-        # cleat the "temp" variable!
-        rm(temp)
+        dataset_temp <- as.data.table(HAR_final[[i]])
+        dataset_temp <- dataset_temp[, lapply(.SD, mean), by = activityname]
+        HAR_dataset3 <- rbind(HAR_dataset3,dataset_temp)
+        # cleat the "temp" dataset!
+        rm(dataset_temp)
 }
-# HAR_dataset3 <- merge(activity_labels,HAR_dataset3)
-# HAR_dataset4 <- c()
-# HAR_dataset4 <- lapply(HAR_final, mean)
-
-        
+#
+# Write run_analisys_dataset.txt file 
+#
+write.table(HAR_dataset3, file = "runanalisys_dataset.txt",row.names=FALSE, sep="\t")
+#
